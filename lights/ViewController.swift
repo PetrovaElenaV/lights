9d682f95819e0bb289415068d9eddd1177f2838e
Elena Petrova <e_v_b@mail.ru>
2023-01-31 17:13:23 +0300

    created round traffic lights

---
+++ lights/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  lights
+//
+//  Created by Elena Petrova on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var redLightsView: UIView!
+    @IBOutlet var yellowLightsView: UIView!
+    @IBOutlet var greenLightsView: UIView!
+    
+    @IBOutlet var launchLightsButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redLightsView.layer.cornerRadius = 90
+        yellowLightsView.layer.cornerRadius = 90
+        greenLightsView.layer.cornerRadius = 90
+        
+        launchLightsButton.layer.cornerRadius = 15
+        
+    }
+    
+    @IBAction func launchLightsButtonDidTapped() {
+        
+    }
+    
+}
+
